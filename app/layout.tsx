7701bddbabc7afc6bf96ef353331594d96ed52c2
Aishik Dasgupta <itsdg19@gmail.com>
2025-04-15 15:34:20 +0530

    website name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Kohinnor Creative World',
-  description: 'Kohinnor Creative is a platform for showcasing and celebrating Haryanvi art and culture.',
+  title: 'Kohinnoor Creative World',
+  description: 'Kohinnoor Creative is a platform for showcasing and celebrating Haryanvi art and culture.',
 };
 
 export default function RootLayout({
