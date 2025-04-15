9010ac8ae0552c35dcbc6d1e901d6b847128b4c1
Aishik Dasgupta <itsdg19@gmail.com>
2025-04-15 15:50:54 +0530

    favicon added again

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Kohinnoor Creative World',
   description: 'Kohinnoor Creative is a platform for showcasing and celebrating Haryanvi art and culture.',
+  icons: "public/favicon.png"
 };
 
 export default function RootLayout({
