3cb6670a6dcbcf017c6209a845ab8803d64a8fac
Aishik Dasgupta <itsdg19@gmail.com>
2025-04-15 16:17:31 +0530

    Refactor layout metadata and improve description for clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Kohinnoor Creative World',
-  description: 'Kohinnoor Creative is a platform for showcasing and celebrating Haryanvi art and culture.',
+  title: "Kohinnoor Creative World",
+  description:
+    "Through events, workshops, and digital initiatives, we aim to make Haryanvi art and culture accessible to both local communities and global audiences.",
   icons: {
     icon: "/favicon.ico",
   },
@@ -19,9 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
